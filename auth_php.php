765add396f14ff6e627aef649ba1c9cbfd45eb78
Justin <yuting1987@gmail.com>
2013-09-15 13:47:15 +0800

    Update

--- auth_php.php
+++ auth_php.php
@@ -1,4 +1,12 @@
 <?php
   $port = exec('/sbin/getcfg System "Web Access Port" -d 8080 -f /etc/config/uLinux.conf');
-  $loginURL = "http://127.0.0.1:".$port."/cgi-bin/authLogin.cgi?sid=".$_COOKIE['NAS_SID']."&service=100";
+  $loginURL = "http://127.0.0.1:".$port."/cgi-bin/authLogin.cgi?user=admin&pwd=YWRtaW4%3D";
+  //$loginURL = "http://127.0.0.1:".$port."/cgi-bin/authLogin.cgi?sid=".$_COOKIE['NAS_SID']."&service=100";
   $root = simplexml_load_file($loginURL);
+  echo $root->errorValue;
+  echo "\n";
+  echo $root->authSid;
+  echo "\n";
+  echo $root->username;
+  echo "\n";
+  //Base64.encode
