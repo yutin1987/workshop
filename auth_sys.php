dc7b4d3ea41509990e9939eab97e756b935ef6fa
Justin <yuting1987@gmail.com>
2013-09-15 13:57:01 +0800

    Update

--- auth_sys.php
+++ auth_sys.php
@@ -1,4 +1,7 @@
 <?php
+$php_user = $_SERVER['PHP_AUTH_USER'];
+$php_pw = $_SERVER['PHP_AUTH_PW'];
+
 $php_shadow=file_get_contents('/etc/shadow');
 preg_match('/admin:([^:]+)/',$php_shadow,$php_admin);
 $php_admin = $php_admin[1];
@@ -6,4 +9,6 @@ if ($php_admin!==crypt($php_pw, $php_admin)) {
     header('WWW-Authenticate: Basic realm="NAS"');
     header('HTTP/1.0 401 Unauthorized');
     exit;
+}else{
+  echo 'PASS';
 }
